487f7fd8c65ab4e06959d7e687f2949bc8af96b4
Dominik <dominik@cloud-architect.io>
2021-05-05 18:01:50 +0200

    Adjusted frame rate constraints

--- src/content/getusermedia/resolution/js/main.js
+++ src/content/getusermedia/resolution/js/main.js
@@ -73,13 +73,19 @@ const hdConstraints = {
 };
 
 const fullHdConstraints = {
-    video: { width: { exact: 1920 }, height: { exact: 1080 } },
-    frameRate: { min: 15, ideal: 60, max: 60 },
+    video: {
+        width: { exact: 1920 },
+        height: { exact: 1080 },
+        frameRate: { min: 15, ideal: 60, max: 60 },
+    },
 };
 
 const televisionFourKConstraints = {
-    video: { width: { exact: 3840 }, height: { exact: 2160 } },
-    frameRate: { min: 15, ideal: 60, max: 60 },
+    video: {
+        width: { exact: 3840 },
+        height: { exact: 2160 },
+        frameRate: { min: 15, ideal: 60, max: 60 },
+    },
 };
 
 const cinemaFourKConstraints = {
